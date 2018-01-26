16fdb4118a2b7037202697e49ca3c7776f985c53
ZeusLMT <lemanhtuan275@gmail.com>
2018-01-26 15:34:52 +0200

    Added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ListMediaComponent } from './list-media/list-media.component';
 import {MediaService} from './services/media.service';
@@ -15,7 +15,7 @@ import {DigitransitService} from './services/digitransit.service';
     ListMediaComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, NgbModule.forRoot()
   ],
   providers: [MediaService, DigitransitService],
   bootstrap: [AppComponent]
